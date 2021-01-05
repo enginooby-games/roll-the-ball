7aa19042f0f63d1fd6252fa97ed7022edcb6b50c
Hieu Ngo <justacpulover@gmail.com>
2021-01-05 16:14:07 +0100

    Add speed component & control system for the ball

---
+++ Assets/Scripts/Systems/MovementSystem.cs
@@ -0,0 +1,21 @@
+﻿using Unity.Burst;
+using Unity.Collections;
+using Unity.Entities;
+using Unity.Jobs;
+using Unity.Mathematics;
+using Unity.Physics;
+using Unity.Transforms;
+using UnityEngine;
+
+public class MovementSystem : SystemBase
+{
+    protected override void OnUpdate()
+    {
+        float deltaTime = Time.DeltaTime;
+        float2 moveInput = new float2(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"));
+
+        Entities.ForEach((ref PhysicsVelocity vel, in SpeedData speedData) => {
+            vel.Linear.xz +=moveInput* speedData.speed * deltaTime;
+        }).Schedule();
+    }
+}
